290cb78d49d67f4ed7d83c25cab1a668ef849932
Shawn Lee <changoloco.evg@gmail.com>
2022-12-18 13:08:15 -0600

    chore(deps): add unocss

--- apps/svelte/src/main.ts
+++ apps/svelte/src/main.ts
@@ -1,4 +1,4 @@
-import "./app.css";
+import "uno.css";
 import App from "./App.svelte";
 
 const app = new App({
--- apps/svelte/vite.config.ts
+++ apps/svelte/vite.config.ts
@@ -1,6 +1,7 @@
+import unocss from "unocss/vite";
 import { defineConfig } from "vite";
 import { svelte } from "@sveltejs/vite-plugin-svelte";
 
 export default defineConfig({
-  plugins: [svelte()],
+  plugins: [svelte(), unocss()],
 });
