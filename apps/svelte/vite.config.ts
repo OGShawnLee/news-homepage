6964918ac1604859fd30d4e5bf0a39be6ce6fd13
Shawn Lee <changoloco.evg@gmail.com>
2022-12-18 20:56:34 -0600

    chore(alias): add lib path alias

--- apps/svelte/vite.config.ts
+++ apps/svelte/vite.config.ts
@@ -1,7 +1,14 @@
 import unocss from "unocss/vite";
 import { defineConfig } from "vite";
 import { svelte } from "@sveltejs/vite-plugin-svelte";
+import { resolve } from 'path'
 
 export default defineConfig({
   plugins: [svelte(), unocss()],
+  resolve: {
+    alias: {
+      "@lib": resolve("src/lib"),
+      "@lib/*": resolve("src/lib/*"),
+    }
+  }
 });
